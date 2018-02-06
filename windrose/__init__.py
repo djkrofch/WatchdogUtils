165bc12a39c8862f942b80d6a81f1cd3eb075b3e
djkrofch <Krofcheck, D. J>
2018-02-06 12:31:39 -0700

    Added windrose for wind direction and velocity visuals

---
+++ windrose/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from .windrose import WindAxesFactory, WindroseAxes, WindAxes, \
+    wrcontour, wrcontourf, wrbox, wrbar, wrpdf, wrscatter, \
+    clean, clean_df, \
+    plot_windrose_np, plot_windrose_df, plot_windrose, \
+    FIGSIZE_DEFAULT, DPI_DEFAULT, D_KIND_PLOT
